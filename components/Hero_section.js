11fb7d087ba00f357df58eae08cba69df498cfb3
Paulo Custodio <pcustodio.mail@gmail.com>
2021-02-24 15:31:13 -0300

    teste

--- components/Hero_section.js
+++ components/Hero_section.js
@@ -9,7 +9,7 @@ class Hero_section extends Component {
                 
                     <div class="grid grid-cols-8 grid-rows-4  " >
                     <h1 class="  shadow-lg col-start-2 col-end-4 row-start-2 row-end-2  bg-black  text-gray-100 text-3xl px-6 py-8 xl: ml-8 mr-9  pr-8 xl:col-end-6 xl:-mt-8 xl:-mb-5   sm:pr-52 2xl: mt-6 2xl:mr-96 pl-20  ">
-                            Sua equipe de e-commerce remota flexível e eficiente
+                            Sua equipe de e-commerce remota flexível e eficiente 
                         </h1>
                       {/* call to action */}
                         <div class=" bg-gray-800 row-start-4 row-end-4 col-start-3 col-end-9  h-24 lg:mt-16   ">
