1adc1b7fe660d69a44770e1d3645dedcbaf5df2f
Paulo César <66526910+paulocesarcustodio@users.noreply.github.com>
2021-02-25 22:13:04 -0300

    Update Team_section.js

--- components/Team_section.js
+++ components/Team_section.js
@@ -1,65 +1,95 @@
-import React, { Component } from 'react'
-
-
+import React, { Component } from "react";
 
 class Team_section extends Component {
-    render() {
-        return (
-            <div class="bg-gray-900 pb-48  ">
-                 
-                <h1 class="text-gray-100 text-4xl text-center p-20 " >Equipe</h1>
+  render() {
+    return (
+      <div class="bg-gray-900 pb-32  ">
+        <h1 class="text-gray-100 text-4xl text-center p-20 ">Equipe</h1>
 
-                {/* team members */}
-                <div class=" 2xl:ml-96 ">
-                <div class="flex items-center lg:mr-28 lg:ml-28 2xl:mx-80 2xl:my-2 ">
-                    <figure class="text-center ">
-                        <img class=" xl:w-80 2xl:w-60  " src="/assets/augusto.png" alt="augusto avatar" />
-                        <figcaption class=" text-gray-100  text-3xl text-center pt-3 ">Augusto</figcaption>
-
-            </figure>
-                    <p class=" p-10 text-gray-100 2xl:w-1/2">Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique, dolore explicabo, fuga rem non eligendi nemo voluptate corrupti, aliquam nostrum sint mollitia impedit reiciendis facere quis fugiat debitis velit laborum!</p>
-                    
-                    
-                  
-                </div>
-
- <div class="flex items-center lg:pt-16 lg:mr-28 lg:ml-28 2xl:mx-80  ">
-                    <p class=" p-10 text-gray-100  2xl:w-1/2">Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique, dolore explicabo, fuga rem non eligendi nemo voluptate corrupti, aliquam nostrum sint mollitia impedit reiciendis facere quis fugiat debitis velit laborum!</p>
-                    <figure class="text-center ">
-                        <img class=" xl:w-80 2xl:w-60  " src="/assets/Lethicia.png" alt="Lethicia avatar" />
-                        <figcaption class=" text-gray-100  text-3xl text-center pt-3 ">Lethicia</figcaption>
-
-            </figure>
-                    
-                    
-                  
-                </div>
-                
-                 <div class="flex items-center lg:mr-28 lg:ml-28 2xl:ml-64 2xl:mt-16 pt-16 2xl:-pt-32 ">
-                    <figure class="text-center ">
-                        <img class=" xl:w-80 2xl:w-60  " src="/assets/Paulo.png" alt="Paulo avatar" />
-                        <figcaption class=" text-gray-100  text-3xl text-center pt-3 ">Paulo</figcaption>
-
-            </figure>
-                    <p class=" p-10 text-gray-100  2xl:w-1/2">Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique, dolore explicabo, fuga rem non eligendi nemo voluptate corrupti, aliquam nostrum sint mollitia impedit reiciendis facere quis fugiat debitis velit laborum!</p>
-                    
-                    
-                  
-                </div>
-                <div class="flex items-center pt-10 lg:mr-28 lg:ml-28 2xl:ml-28 2xl:mr-96  2xl:mt-16 ">
-                    <p class=" p-10 text-gray-100  2xl:w-1/2 2xl:ml-48">Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique, dolore explicabo, fuga rem non eligendi nemo voluptate corrupti, aliquam nostrum sint mollitia impedit reiciendis facere quis fugiat debitis velit laborum!</p>
-                    <figure class="text-center ">
-                        <img class=" xl:w-80 2xl:w-60  " src="/assets/Yara.png" alt="Yara Avatar" />
-                        <figcaption class=" text-gray-100  text-3xl text-center pt-3 ">Yara</figcaption>
-
-            </figure>
-                    
-                    
-                    </div>
-                    </div>
+        {/* team members */}
+        <center>
+          <div class=" flex flex-col w-4/5 lg:w-3/5 p-5 2xl:w-1/3">
+            <div class=" pb-20 flex flex-col lg:flex-row">
+              <figure class="text-center ">
+                <img
+                  class="w-42 pl-5 lg:pr-5 lg:w-56 xl:w-56 2xl:56"
+                  src="/assets/augusto.png"
+                  alt="augusto avatar"
+                />
+                <figcaption class=" text-gray-100  text-3xl text-center pt-3 ">
+                  <a href="#">Augusto</a>
+                </figcaption>
+              </figure>
+              <p class="  pt-6 lg:pt-16 text-justify text-gray-100 lg:w-3/5 ">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Similique, dolore explicabo, fuga rem non eligendi nemo
+                voluptate corrupti, aliquam nostrum sint mollitia impedit
+                reiciendis facere quis fugiat debitis velit laborum!
+              </p>
             </div>
-        )
-    }
+
+            <div class=" pb-20 flex flex-col lg:flex-row-reverse">
+              <figure class="text-center ">
+                <img
+                  class="w-42 pl-5 lg:pr-5 lg:w-56 xl:w-56 2xl:56"
+                  src="/assets/Lethicia.png"
+                  alt="Lethicia Avatar"
+                />
+                <figcaption class=" text-gray-100  text-3xl text-center pt-3 ">
+                  <a href="#">Lethicia</a>
+                </figcaption>
+              </figure>
+              <p class="  pt-6 lg:pt-16 text-justify text-gray-100 lg:w-3/5 ">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Similique, dolore explicabo, fuga rem non eligendi nemo
+                voluptate corrupti, aliquam nostrum sint mollitia impedit
+                reiciendis facere quis fugiat debitis velit laborum!
+              </p>
+            </div>
+
+            <div class=" pb-20 flex flex-col lg:flex-row">
+              <figure class="text-center ">
+                <img
+                  class="w-42 pl-5 lg:pr-5 lg:w-56 xl:w-56 2xl:56"
+                  src="/assets/Paulo.png"
+                  alt="Paulo Avatar"
+                />
+                <figcaption class=" text-gray-100  text-3xl text-center pt-3 ">
+                  <a href="#">Paulo</a>
+                </figcaption>
+              </figure>
+              <p class="  pt-6 lg:pt-16 text-justify text-gray-100 lg:w-3/5 ">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Similique, dolore explicabo, fuga rem non eligendi nemo
+                voluptate corrupti, aliquam nostrum sint mollitia impedit
+                reiciendis facere quis fugiat debitis velit laborum!
+              </p>
+            </div>
+
+            <div class=" pb-20 flex flex-col lg:flex-row-reverse">
+              <figure class="text-center ">
+                <img
+                  class="w-42 pl-5 lg:pr-5 lg:w-56 xl:w-56 2xl:56"
+                  src="/assets/Yara.png"
+                  alt="Yara Avatar"
+                />
+                <figcaption class=" text-gray-100  text-3xl text-center pt-3 ">
+                  <a href="#">Yara</a>
+                </figcaption>
+              </figure>
+              <p class="  pt-6 lg:pt-16 text-justify text-gray-100 lg:w-3/5 ">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Similique, dolore explicabo, fuga rem non eligendi nemo
+                voluptate corrupti, aliquam nostrum sint mollitia impedit
+                reiciendis facere quis fugiat debitis velit laborum!
+              </p>
+            </div>
+          </div>
+        </center>
+      </div>
+    );
+  }
 }
 
-export default Team_section
+export default Team_section;
+
